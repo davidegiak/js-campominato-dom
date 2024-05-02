02312a259c1353dfdb74fbc882b8ad2c7abbd76f
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-02 16:09:49 +0200

    ho creato le bombe e le individuo al click sulla casella

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 let griglia = document.querySelector(".container");
 let select = document.querySelector("#select");
 const btn = document.querySelector("#btn");
-
+let bombs =[];
+let mina = 1;
+// ************/AL CLICK DEL BOTTONE SI GENERANO LE GRIGLIE\*****************
 btn.addEventListener("click", function(){
     griglia.innerHTML ="";
     if (select.value == "easy"){
         for (let i = 1; i <= 100; i++) {
             let casella = quadrato(i)
             griglia.append(casella);
-    }
+            
+        }
+// ***********CREO LE BOMBE DA APPLICARE SULLE CASELLE AUTOMATICAMENTE AL CLICK DEL BOTTONE
+  
+      while (bombs.length < 16) {
+        mina = Math.floor(Math.random() * 100) + 1;
+        if (mina != bombs.values){
+            bombs.push(mina);
+            console.log(mina)
+
+        }
+      }
     }
 
-//*****************************/ PARTE DEL SELECT \*******************
+//*****************************/ PARTE DELLE DIFFICOLTA' \*******************
     // else if(select.value == "medium"){
     //     for (let i = 1; i <= 81; i++) {
     //         let casella = quadrato(i)
@@ -31,6 +44,7 @@ btn.addEventListener("click", function(){
     // }
 //  *********************************************************************\
     
+// **********************/ FUNZIONE PER LA CREAZIONE DEL QUADRATO \*********************
     
     function quadrato(contenuto) {
         let square = document.createElement("div");
@@ -41,19 +55,22 @@ btn.addEventListener("click", function(){
                 square.append(contenuto);
                 square.classList.add("white");
                 square.classList.remove("red");
+                if (bombs.includes(contenuto)) {
+                    square.classList.add("bomba")
+                }
             }
-            else if (square.classList.contains("red", "squareMed")){
-                square.append(contenuto);
-                square.classList.remove("square");
-                square.classList.add("white");
-                square.classList.remove("red");
-            }
-            else if (square.classList.contains("red", "squareHard")){
-                square.append(contenuto);
-                square.classList.remove("square");
-                square.classList.add("white");
-                square.classList.remove("red");
-            }
+            // else if (square.classList.contains("red", "squareMed")){
+            //     square.append(contenuto);
+            //     square.classList.remove("square");
+            //     square.classList.add("white");
+            //     square.classList.remove("red");
+            // }
+            // else if (square.classList.contains("red", "squareHard")){
+            //     square.append(contenuto);
+            //     square.classList.remove("square");
+            //     square.classList.add("white");
+            //     square.classList.remove("red");
+            // }
         } )
         return square
     }
